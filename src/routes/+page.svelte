290a4ab4c2231718a1c479d5be249c6bf5613785
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2024-01-20 11:04:46 +0530

    added all blogs button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Blogcard, Masonry, LatestPost } from '$lib/components/site';
+	import { Button } from '$lib/components/ui/button';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	const likes = data.likes;
@@ -8,14 +9,15 @@
 <div class="md:mb-20 md:mt-12">
 	<LatestPost post={data.posts[0]} />
 
-	<div class="flex items-center justify-center">
-		<div class="my-7">
+	<div class="flex flex-col items-center justify-center gap-3">
+		<div class="mt-5">
 			<h1 class="text-4xl font-bold tracking-widest">Recent Blogs</h1>
 		</div>
+		<Button href="/blog">All Blogs</Button>
 	</div>
 
 	<div class="mt-7 max-w-7xl gap-x-[5px] md:mx-auto">
-		<Masonry gridGap={'0.75rem'}  stretchFirst={false} reset>
+		<Masonry gridGap={'0.75rem'} stretchFirst={false} reset>
 			{#each data.posts as post}
 				<Blogcard
 					{post}
